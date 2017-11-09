7bfd6ed41d3df97c0b140d21af1f72b898b608af
Lucas Andrade <luksandrades18@gmail.com>
2017-11-09 20:37:38 -0200

    Update sendMail.php

--- sendMail.php
+++ sendMail.php
@@ -1,5 +1,36 @@
 <?php
 
-    if(isset($_POST["email"])){
-        echo $_POST["email"];
+    require("phpmailer/class.phpmailer.php");
+
+    if(!isset($_POST["email"]) || !isset($_POST["nome"]){
+        echo "Dados invalidados";
+        header( "refresh:5; url=index.php" );
+    }
+
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+       
+    $mail = new PHPMailer(true);
+    $mail->IsSMTP();
+    try{
+        $mail->Host = 'smtp.gmail.com';//Servidor SMTP
+        $mail->SMTPAuth = true;//Usa a autenticação SMTP
+        $mail->SMTPSecure = 'ssl';
+        $mail->SMTPDebug = 1;
+        $mail->Port = 465;//Porta de conexão SMTP
+        $mail->Username = '';//Usario do seervidor
+        $mail->Password = '';
+        //Define o remetente
+        $mail->SetFrom('unievangelicasites@gmail.com', 'SITES');
+        //$mail->AddReplyTo('recrutadorsysrecruit@gmail.com', $nome);;
+        $mail->Subject = 'CONFIRMAÇÂO SITES';
+        //Define o detinatário
+        $mail->AddAddress('$email', 'SITES');
+        $mail->MsgHTML('Nome: '.$nome.'<br/><br/>'.'E-Mail: '.$email.'<br/>');
+        $mail->Send();
+        echo "Enviado com sucesso";
+        header( "refresh:5; url=index.php" );
+    } catch(phpmailerException $e){
+        echo $e->errorMessage();
+        header( "refresh:5; url=index.php" );
     }
