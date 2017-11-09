e05c532ae3f11dd67a61e16c4f75254dbe928896
Lucas Andrade <luksandrades18@gmail.com>
2017-11-09 20:25:55 -0200

    Update index.php

--- index.php
+++ index.php
@@ -294,15 +294,15 @@
             <br/>
             <div class="row justify-content-center">
                 <div class="col-12 col-sm-4 col-xl-4">
-                    <form id="newsletter" style="text-align: justify; color: #fff">
+                    <form method="post" action="sendMail.php" id="newsletter" style="text-align: justify; color: #fff">
                         <p class="lead">Cadastre-se para receber novidades!</p>
                         <div class="form-group">
                             <label for="nome">Nome</label>
-                            <input type="text" class="form-control" id="nome" placeholder="Nome" required>                        
+                            <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" required>                        
                         </div>
                         <div class="form-group">
                             <label for="email">E-mail</label>
-                            <input type="email" class="form-control" id="email" placeholder="E-mail" required>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="E-mail" required>
                         </div>
                         <p class="lead col-12 bg-success white-text confirmacao" style="text-align: center; border-radius: 5px;">Enviado com sucesso!!!</p>
                         <button type="submit" class="btn col-12 btn-primary">Enviar</button>
@@ -325,13 +325,14 @@
                     <p class="lead">Youtube</p>
                 </div>
             </div>
+            <br>
             <p class="lead" style="color: #fff; font-size: 30px"><b>#SitesUnievangelica</b></p>
             <br/>
             <br/>
             <br/>
         </div>
         <br/>
-        <p class="lead" style="text-align: center"><b>Copyright &copy; 2017 Engenharia de Computação. Todos os Direitos Reservados</b></p>
+        <p class="lead" style="text-align: center; font-size: 15px"><b>Copyright &copy; 2017 Engenharia de Computação. Todos os Direitos Reservados</b></p>
 
         <!--Modal's-->
 
@@ -472,34 +473,34 @@
                         <br/>
                         <div class="row justify-content-center" style="text-align: center">
                             <div class="col-12 col-sm-4 col-xl-3">
-                                <i class="icofont icofont-file-excel minicursos"></i>
+                                <a href="http://ead.unievangelica.edu.br/excel/inscricao/index.php?idCurso=35&idEdicao=109" target="blank" style="text-decoration: none"><i class="icofont icofont-file-excel minicursos"></i></a>
                                 <p class="lead"><b>Excel</b></p>
                             </div>
                             <div class="col-12 col-sm-4 col-xl-3">
-                                <i class="icofont icofont-file-word minicursos"></i>
+                                <a href="http://ead.unievangelica.edu.br/word/inscricao/index.php?idCurso=35&idEdicao=110" target="blank" style="text-decoration: none"><i class="icofont icofont-file-word minicursos"></i></a>
                                 <p class="lead"><b>Word</b></p>
                             </div>
                             <div class="col-12 col-sm-4 col-xl-3">
-                                <i class="icofont icofont-tools-alt-2 minicursos"></i>
+                                <a href="http://ead.unievangelica.edu.br/manutencaocomputadores/inscricao/index.php?idCurso=35&idEdicao=111" target="blank" style="text-decoration: none"><i class="icofont icofont-tools-alt-2 minicursos"></i></a>
                                 <p class="lead"><b>Manutenção de Computadores</b></p>
                             </div>
                         </div>
                         <br/>
                         <div class="row justify-content-center" style="text-align: center">
                             <div class="col-12 col-sm-3 col-xl-3">
-                                <i class="icofont icofont-ui-cart minicursos"></i>
+                                <a href="http://ead.unievangelica.edu.br/ecommerce/inscricao/index.php?idCurso=35&idEdicao=113" target="blank" style="text-decoration: none"><i class="icofont icofont-ui-cart minicursos"></i></a>
                                 <p class="lead"><b>E-commerce</b></p>
                             </div>
                             <div class="col-12 col-sm-3 col-xl-3">
-                                <i class="icofont icofont-file-image minicursos"></i>
+                                <a href="http://ead.unievangelica.edu.br/edicaoimagensfilmadora/inscricao/index.php?idCurso=35&idEdicao=114" target="blank" style="text-decoration: none"><i class="icofont icofont-file-image minicursos"></i></a>
                                 <p class="lead"><b>Edição de Imagens - Filmora</b></p>
                             </div>
                             <div class="col-12 col-sm-3 col-xl-3">
-                                <i class="icofont icofont-social-opencart minicursos"></i>
+                                <a href="http://ead.unievangelica.edu.br/marketingdigital/inscricao/index.php?idCurso=35&idEdicao=115" target="blank" style="text-decoration: none"><i class="icofont icofont-social-opencart minicursos"></i></a>
                                 <p class="lead"><b>Marketing Digital</b></p>
                             </div>
                             <div class="col-12 col-sm-3 col-xl-3">
-                                <i class="icofont icofont-camera minicursos"></i>
+                                <a href="http://ead.unievangelica.edu.br/photoshop/inscricao/index.php?idCurso=35&idEdicao=116" target="blank" style="text-decoration: none"><i class="icofont icofont-camera minicursos"></i></a>
                                 <p class="lead"><b>Photoshop</b></p>
                             </div>
                         </div>
