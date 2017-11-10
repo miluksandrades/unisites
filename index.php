28fc57938bcdbe9824ac90ce94f1eacd21deac74
Lucas Andrade <luksandrades18@gmail.com>
2017-11-10 14:56:29 -0200

    Correção dos links do minicurso

--- index.php
+++ index.php
@@ -473,34 +473,34 @@
                         <br/>
                         <div class="row justify-content-center" style="text-align: center">
                             <div class="col-12 col-sm-4 col-xl-3">
-                                <a href="http://ead.unievangelica.edu.br/excel/inscricao/index.php?idCurso=35&idEdicao=109" target="blank" style="text-decoration: none"><i class="icofont icofont-file-excel minicursos"></i></a>
+                                <a href="http://ead.unievangelica.edu.br/excel/inscricao/index.php?idCurso=35&idEdicao=109" target="_blank" style="text-decoration: none"><i class="icofont icofont-file-excel minicursos"></i></a>
                                 <p class="lead"><b>Excel</b></p>
                             </div>
                             <div class="col-12 col-sm-4 col-xl-3">
-                                <a href="http://ead.unievangelica.edu.br/word/inscricao/index.php?idCurso=35&idEdicao=110" target="blank" style="text-decoration: none"><i class="icofont icofont-file-word minicursos"></i></a>
+                                <a href="http://ead.unievangelica.edu.br/word/inscricao/index.php?idCurso=35&idEdicao=110" target="_blank" style="text-decoration: none"><i class="icofont icofont-file-word minicursos"></i></a>
                                 <p class="lead"><b>Word</b></p>
                             </div>
                             <div class="col-12 col-sm-4 col-xl-3">
-                                <a href="http://ead.unievangelica.edu.br/manutencaocomputadores/inscricao/index.php?idCurso=35&idEdicao=111" target="blank" style="text-decoration: none"><i class="icofont icofont-tools-alt-2 minicursos"></i></a>
+                                <a href="http://ead.unievangelica.edu.br/manutencaocomputadores/inscricao/index.php?idCurso=35&idEdicao=111" target="_blank" style="text-decoration: none"><i class="icofont icofont-tools-alt-2 minicursos"></i></a>
                                 <p class="lead"><b>Manutenção de Computadores</b></p>
                             </div>
                         </div>
                         <br/>
                         <div class="row justify-content-center" style="text-align: center">
                             <div class="col-12 col-sm-3 col-xl-3">
-                                <a href="http://ead.unievangelica.edu.br/ecommerce/inscricao/index.php?idCurso=35&idEdicao=113" target="blank" style="text-decoration: none"><i class="icofont icofont-ui-cart minicursos"></i></a>
+                                <a href="http://ead.unievangelica.edu.br/ecommerce/inscricao/index.php?idCurso=35&idEdicao=113" target="_blank" style="text-decoration: none"><i class="icofont icofont-ui-cart minicursos"></i></a>
                                 <p class="lead"><b>E-commerce</b></p>
                             </div>
                             <div class="col-12 col-sm-3 col-xl-3">
-                                <a href="http://ead.unievangelica.edu.br/edicaoimagensfilmadora/inscricao/index.php?idCurso=35&idEdicao=114" target="blank" style="text-decoration: none"><i class="icofont icofont-file-image minicursos"></i></a>
+                                <a href="http://ead.unievangelica.edu.br/edicaoimagensfilmadora/inscricao/index.php?idCurso=35&idEdicao=114" target="_blank" style="text-decoration: none"><i class="icofont icofont-file-image minicursos"></i></a>
                                 <p class="lead"><b>Edição de Imagens - Filmora</b></p>
                             </div>
                             <div class="col-12 col-sm-3 col-xl-3">
-                                <a href="http://ead.unievangelica.edu.br/marketingdigital/inscricao/index.php?idCurso=35&idEdicao=115" target="blank" style="text-decoration: none"><i class="icofont icofont-social-opencart minicursos"></i></a>
+                                <a href="http://ead.unievangelica.edu.br/marketingdigital/inscricao/index.php?idCurso=35&idEdicao=115" target="_blank" style="text-decoration: none"><i class="icofont icofont-social-opencart minicursos"></i></a>
                                 <p class="lead"><b>Marketing Digital</b></p>
                             </div>
                             <div class="col-12 col-sm-3 col-xl-3">
-                                <a href="http://ead.unievangelica.edu.br/photoshop/inscricao/index.php?idCurso=35&idEdicao=116" target="blank" style="text-decoration: none"><i class="icofont icofont-camera minicursos"></i></a>
+                                <a href="http://ead.unievangelica.edu.br/photoshop/inscricao/index.php?idCurso=35&idEdicao=116" target="_blank" style="text-decoration: none"><i class="icofont icofont-camera minicursos"></i></a>
                                 <p class="lead"><b>Photoshop</b></p>
                             </div>
                         </div>
@@ -563,7 +563,7 @@
                                 Interdisciplinares de Tecnologia e Sociedade - SITES. É utilizada como estratégia
                                 motivacional cujo desígnio é fortalecer a percepção dos alunos quanto aos projetos que
                                 são desenvolvidos ao longo das disciplinas de Projeto Interdisciplinar.</b></p>
-                        <p class="lead"><b>Edital do Jogo da Hora: <a href="files/XVII_SITES_Edital_Jogo_Da_Hora.pdf" target="blank" style="color: red;text-decoration: none">Edital</a></b></p>
+                        <p class="lead"><b>Edital do Jogo da Hora: <a href="files/XVII_SITES_Edital_Jogo_Da_Hora.pdf" target="_blank" style="color: red;text-decoration: none">Edital</a></b></p>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
@@ -619,7 +619,7 @@
                             </div>                            
                         </div>
                         <br/>
-                        <p class="lead"><a href="files/edital-robocode.pdf" target="blank" style="color: red; text-decoration: none">Clique aqui</a> para visualizar o regulamento da competição.</p>
+                        <p class="lead"><a href="files/edital-robocode.pdf" target="_blank" style="color: red; text-decoration: none">Clique aqui</a> para visualizar o regulamento da competição.</p>
                         <p class="lead">OBS.: Clique em inscrição robocode para cadastrar sua equipe.</p>
                     </div>
                     <div class="modal-footer">
