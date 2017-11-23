a12e89aa903e753f5d8613d99680c2a6ae2f6c00
samuka701 <samdev@outlook.com.br>
2017-11-23 14:09:33 -0200

    Html encode
    Html encode

--- robocode/unsubscription_message.php
+++ robocode/unsubscription_message.php
@@ -7,9 +7,9 @@
         if($result_canc == 'cancel'){
             echo '<h2>Cancelamento de Inscri&ccedil;&atilde;o Realizado com sucesso!</h2>';
         }else if($result_canc == 'notfound'){
-            echo '<h2>Token de cancelamento não encontrado.</h2>';
+            echo '<h2>Token de cancelamento n&atilde;o encontrado.</h2>';
         }
     ?>
-    Você ser&aacute; redirecionado em instantes.
+    Voc&ecirc; ser&aacute; redirecionado em instantes.
 </body>
 </html>
